844c6e7735e489eab3fab48ab191bc261874dc5c
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-06-08 13:00:43 +0200

    return random 1MB base64 payload on ping

--- src/resources/ping.rs
+++ src/resources/ping.rs
@@ -1,6 +1,7 @@
 use crate::models::response_models::MessageResponse;
 use axum::response::{IntoResponse, Response};
 use axum::{routing::get, Json, Router};
+use rand::Rng;
 
 /// Ping the API for a response.
 ///
@@ -9,16 +10,22 @@ use axum::{routing::get, Json, Router};
     get,
     path = "/",
     responses(
-        (status = 200, description = "Pong", body = MessageResponse),
+        (status = 200, description = "Random 1MB payload", body = MessageResponse),
         (status = 500, description = "Server error"),
     ),
     tag = "Misc"
 )]
+#[allow(deprecated)]
 async fn get_ping() -> Response {
+    let mut rng = rand::thread_rng();
+    let payload: Vec<u8> = (0..1024 * 1024).map(|_| rng.gen()).collect();
+    let base64_payload = base64::encode(payload);
+
     let response = MessageResponse {
-        message: "Pong".to_string(),
+        message: base64_payload,
     };
-    println!("Request received: PING");
+
+    println!("Request received: PING with 1MB payload");
     Json(response).into_response()
 }
 
